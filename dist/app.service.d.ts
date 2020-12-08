01fd3735b4f88ecb5276ec8092c87d3eb64490e8
Artsiom <artsiom_peshko@epam.com>
2020-12-08 20:31:35 +0300

    update giringore

--- dist/app.service.d.ts
+++
@@ -1,6 +0,0 @@
-import { RedirectService } from './redirect.service';
-export declare class AppService {
-    private readonly redirectService;
-    constructor(redirectService: RedirectService);
-    processRequest(request: Request, body: any): Promise<any>;
-}
--- dist/redirect.service.d.ts
+++
@@ -1,6 +0,0 @@
-import { ConfigService } from '@nestjs/config';
-export declare class RedirectService {
-    private configService;
-    constructor(configService: ConfigService);
-    getServiceUrl(url: string): string;
-}
